46a17a2d6f3610f60c9e1e226deb08cf47e65b9c
Mariano Murcia Ortuño <marmurort@alu.edu.gva.es>
2023-10-20 18:12:38 +0200

    Ultima version consola

--- src/Consola.java
+++ src/Consola.java
@@ -3,35 +3,20 @@ import java.util.Scanner;
 
 public class Consola {
     public static void main(String[] args) {
-        ProcessBuilder pb = new ProcessBuilder("bash");
-//        pb.inheritIO();
+        ProcessBuilder pb = new ProcessBuilder("zsh");
+        pb.inheritIO();
         Scanner entrada = new Scanner(System.in);
-        String comando;
         try {
             Process p = pb.start();
             while (p.isAlive()) {
-                try (InputStream is = p.getInputStream();
-                     InputStreamReader isr = new InputStreamReader(is);
-                     BufferedReader br = new BufferedReader(isr)) {
-                    String linea;
-                    while ((linea = br.readLine()) != null) {
-                        System.out.print("\n" + linea);
-
-                    }
-                }
-                try (OutputStream os = p.getOutputStream();
-                     OutputStreamWriter osr = new OutputStreamWriter(os)) {
-                    System.out.print("Introduce comando:");
-                    comando = entrada.nextLine();
-                    if (!comando.equals("exit")) {
-                        osr.write(comando);
-                        osr.flush();
-                    }
-                }
+                entrada.nextLine();
             }
-
-                } catch (IOException e) {
-                    System.out.println("Error en el proceso: " + e.getMessage());
-                }
-            }
+            int codigoRetorno = p.waitFor();
+            System.out.println("El proceso ha finalizado con el codigo: " + codigoRetorno);
+        } catch (IOException e) {
+            System.out.println("Error en el proceso: " + e.getMessage());
+        }catch (InterruptedException e){
+            System.out.println("El proceso se ha interrumpido: " + e.getMessage());
         }
+    }
+}
